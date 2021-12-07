07ab69738aebeaf1e8ca81563646b84bf108376e
Jafet Diaz <79779934+jaberwooc@users.noreply.github.com>
2021-12-06 17:57:54 -0800

    Update programa2.s

--- codigos/programa2.s
+++ codigos/programa2.s
@@ -1,4 +1,8 @@
-
+/* Problema 2.2
+  En este programa lo que se realiza es un if en el cual en esta situacion como se pidio en las instrucciones no se utilizara un else
+   @Autor Jaberwooc
+   @Fecha 6 de diciembre de 2021
+    */
         .arch armv6
         .eabi_attribute 28, 1
         .eabi_attribute 20, 1
