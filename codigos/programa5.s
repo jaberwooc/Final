8ff6d1798782dbf8f854f366c8b4af50ecd00e95
Jafet Diaz <79779934+jaberwooc@users.noreply.github.com>
2021-12-06 17:43:21 -0800

    Update programa5.s

--- codigos/programa5.s
+++ codigos/programa5.s
@@ -1,1 +1,38 @@
-
+    .arch armv6
+        .eabi_attribute 28, 1
+        .eabi_attribute 20, 1
+        .eabi_attribute 21, 1
+        .eabi_attribute 23, 3
+        .eabi_attribute 24, 1
+        .eabi_attribute 25, 1
+        .eabi_attribute 26, 2
+        .eabi_attribute 30, 4
+        .eabi_attribute 34, 1
+        .eabi_attribute 18, 4
+        .file   "t1.c"
+        .text
+        .section        .text.startup,"ax",%progbits
+        .align  2
+        .global main
+        .arch armv6
+        .syntax unified
+        .arm
+        .fpu vfp
+        .type   main, %function
+main:
+        @ args = 0, pretend = 0, frame = 0
+        @ frame_needed = 0, uses_anonymous_args = 0
+        @ link register save eliminated.
+        mov     r1, #104
+        ldr     r0, .L2
+        b       printf
+.L3:
+        .align  2
+.L2:
+        .word   .LC1
+        .size   main, .-main
+        .section        .rodata.str1.1,"aMS",%progbits,1
+.LC1:
+        .ascii  " La suma es %d \012\000"
+         .ident  "GCC: (Raspbian 8.3.0-6+rpi1) 8.3.0"
+        .section        .note.GNU-stack,"",%progbits
